7bf650aa3343a59e1cc53ef3916386e91f908dc4
Joe Wilcoxson <joe.wilcoxson@gmail.com>
2016-05-27 11:31:14 -0400

    Initial Commit

---
+++ OvercastViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OvercastViewer
+//
+//  Created by Joe Wilcoxson on 5/27/16.
+//  Copyright © 2016 Joe Wilcoxson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
